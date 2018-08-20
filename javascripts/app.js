9767d826996d80b1d3106b6a5b5091cc85488016
Francisco Torreblanca <dark1522@gmail.com>
2018-08-20 03:36:38 -0500

    Aumentado el número de rovers a 3. Añadido el comando clear() para emular movimiento

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,17 +1,46 @@
 // Rover Object Goes Here
 // ======================
-var rover = {
+var Sojourner = {
   direccion: "N",
   x: 0,
   y: 0,
   travelLog: ["( 0 , 0 )"],
   befX: 0,
-  befY: 0
+  befY: 0,
+  marca: "S",
+  name: "Sojourner"
 }
+
+var Opportunity = {
+  direccion: "N",
+  x: 1,
+  y: 1,
+  travelLog: ["( 0 , 0 )"],
+  befX: 1,
+  befY: 1,
+  marca: "O",
+  name: "Opportunity"
+}
+
+var Curiosity = {
+  direccion: "N",
+  x: 2,
+  y: 2,
+  travelLog: ["( 0 , 0 )"],
+  befX: 2,
+  befY: 2,
+  marca: "C",
+  name: "Curiosity"
+}
+
+var rovers= [Sojourner, Opportunity, Curiosity];
+var jugadorID = 0;
+
+
 // ======================
 
 var board = [ 
-  ['X',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
@@ -22,142 +51,167 @@ var board = [
   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '] ];
   
-  var obstaculos= generarObstaculos(5);
-  
-console.log(board.join("\n")+"\n\n"); //Muestra el tablero inicial
+generarJugadores();
+var obstaculos= [];
+generarObstaculos(5); //almacena los obstáculos
+
+mostrarTablero();
+
+function generarJugadores() {
+  for(kontador=0;kontador<rovers.length;kontador++){
+    board[rovers[kontador].y][rovers[kontador].x] = rovers[kontador].marca;
+  }
+}
 
 function generarObstaculos(numObst){
-  var obstaculos=[]; //Almacena las coordenadas donde estarán los obstáculos
   for(i=0; i<numObst;i++){
     //Generando obstáculos
     randomCoords: while(true){
-      var coordX=Math.floor(Math.random()*9); 
-      var coordY=Math.floor(Math.random()*9);
-      if(coordX==0 & coordY==0) { continue randomCoords; } //Verifica que el obstáculo no esté en (0,0)
+      var randCoordX=Math.floor(Math.random()*9); 
+      var randCoordY=Math.floor(Math.random()*9);
+      for (contador=0;contador<rovers.length;contador++){
+        if(randCoordX==rovers[contador].x) { 
+          if (randCoordY==rovers[contador].y) {continue randomCoords; }
+         } //Verifica que el obstáculo no esté sobre el jugador
+      }
       for(j=0;j<(obstaculos.length/2);j+=2){
-        if(coordX==obstaculos[j] & coordY ==obstaculos[j+1]) { //Revisa que no se repitan obstáculos
+        if(randCoordX==obstaculos[j] & randCoordY ==obstaculos[j+1]) { //Revisa que no se repitan obstáculos
           continue randomCoords;
         }
       }
       break; 
     }
-    board[coordY][coordX] = '0'; //Representando gráficamente al obstáculo
-    obstaculos.push(coordX,coordY); //Almacenando coordenadas de los obstáculos (cada par de números representa X,Y)
+    board[randCoordY][randCoordX] = '#'; //Representando gráficamente al obstáculo
+    obstaculos.push(randCoordX,randCoordY); //Almacenando coordenadas de los obstáculos (cada par de números representa X,Y)
   }
-  console.log(obstaculos);
-  return obstaculos;
 }
 
-function turnLeft(rover){
+function mostrarTablero() {
+  console.log(board.join("\n")+"\n\n"); //Muestra el tablero inicial
+  console.log("Turno del jugador: "+ rovers[jugadorID].name);
+  console.log("Dirección del rover: " + rovers[jugadorID].direccion);
+}
+
+function turnLeft(){
   //Iteracion para el cambio de direción
-  if(rover.direccion =="N"){rover.direccion="W";}
-  else if(rover.direccion =="W"){rover.direccion="S";}
-  else if(rover.direccion =="S"){rover.direccion="E";}
-  else if(rover.direccion =="E"){rover.direccion="N";}
+  if(rovers[jugadorID].direccion =="N"){rovers[jugadorID].direccion="W";}
+  else if(rovers[jugadorID].direccion =="W"){rovers[jugadorID].direccion="S";}
+  else if(rovers[jugadorID].direccion =="S"){rovers[jugadorID].direccion="E";}
+  else if(rovers[jugadorID].direccion =="E"){rovers[jugadorID].direccion="N";}
 }
 
-function turnRight(rover){
+function turnRight(){
   //Iteracion para el cambio de direcion
-  if(rover.direccion =="N"){rover.direccion="E";}
-  else if(rover.direccion =="W"){rover.direccion="N";}
-  else if(rover.direccion =="S"){rover.direccion="W";}
-  else if(rover.direccion =="E"){rover.direccion="S";}
+  if(rovers[jugadorID].direccion =="N"){rovers[jugadorID].direccion="E";}
+  else if(rovers[jugadorID].direccion =="W"){rovers[jugadorID].direccion="N";}
+  else if(rovers[jugadorID].direccion =="S"){rovers[jugadorID].direccion="W";}
+  else if(rovers[jugadorID].direccion =="E"){rovers[jugadorID].direccion="S";}
 }
 
-function moveForward(rover){
-  rover.befX= rover.x; //Almacena las coordenadas antes de avanzar
-  rover.befY = rover.y; //Almacena las coordenadas antes de avanzar
-  
+function moveForward(){
+  rovers[jugadorID].befX= rovers[jugadorID].x; //Almacena las coordenadas antes de avanzar
+  rovers[jugadorID].befY = rovers[jugadorID].y; //Almacena las coordenadas antes de avanzar
+  console.log("antesX: "+rovers[jugadorID].befX);
+  console.log("antesY: "+rovers[jugadorID].befY);
   //Iteración para avanzar
-  if(rover.direccion =="N"){rover.y--;}
-  else if(rover.direccion =="W"){rover.x--;}
-  else if(rover.direccion =="S"){rover.y++;}
-  else if(rover.direccion =="E"){rover.x++;}
-  
+  if(rovers[jugadorID].direccion =="N"){rovers[jugadorID].y--;}
+  else if(rovers[jugadorID].direccion =="W"){rovers[jugadorID].x--;}
+  else if(rovers[jugadorID].direccion =="S"){rovers[jugadorID].y++;}
+  else if(rovers[jugadorID].direccion =="E"){rovers[jugadorID].x++;}
+
   //Movimiento del rover
-  Movimiento(rover);
+  Movimiento();
 }
 
-function moveBackward(rover){   
-  rover.befX = rover.x; //Almacena las coordenadas antes de avanzar
-  rover.befY = rover.y; //Almacena las coordenadas antes de avanzar
+function moveBackward(){   
+  rovers[jugadorID].befX = rovers[jugadorID].x; //Almacena las coordenadas antes de avanzar
+  rovers[jugadorID].befY = rovers[jugadorID].y; //Almacena las coordenadas antes de avanzar
   
   //Iteración para retroceder
-  if(rover.direccion =="N"){rover.y++;}
-  else if(rover.direccion =="W"){rover.x++;}
-  else if(rover.direccion =="S"){rover.y--;}
-  else if(rover.direccion =="E"){rover.x--;}
+  if(rovers[jugadorID].direccion =="N"){rovers[jugadorID].y++;}
+  else if(rovers[jugadorID].direccion =="W"){rovers[jugadorID].x++;}
+  else if(rovers[jugadorID].direccion =="S"){rovers[jugadorID].y--;}
+  else if(rovers[jugadorID].direccion =="E"){rovers[jugadorID].x--;}
   
   //Movimiento del rover
-  Movimiento(rover);
+  Movimiento();
 }
 
 function comandos(cadenaComandos) {
-  for (m = 0; m < cadenaComandos.length; m++) {
+  for (var m = 0; m < cadenaComandos.length; m++) {
     //Escoger caso según letra en la posición i de la cadena
     switch (cadenaComandos[m]) {
       
       case "f":
-        moveForward(rover);
+        moveForward(rovers[jugadorID]);
       break;
 
       case "l":
-        turnLeft(rover);
+        turnLeft(rovers[jugadorID]);
       break;
       
       case "r":
-        turnRight(rover);
+        turnRight(rovers[jugadorID]);
       break;
       
       case "b":
-        moveBackward(rover);
+        moveBackward(rovers[jugadorID]);
       break;
       
       default:
       break;
     }        
   }
+  jugadorID++;
+  if (jugadorID>=3) { jugadorID=0; }
+  clear();
+  mostrarTablero();
 }
 
-function limites(numX,numY) {
-  if(numX<0){ 
-    numX=0; 
+function limites() {
+  if(rovers[jugadorID].x<0){ 
+    rovers[jugadorID].x=0; 
   }  
-  else if(numX>9){ 
-    numX=9; 
-  }  
-  if(numY<0){ 
-    numY=0; 
+  else if(rovers[jugadorID].x>9){ 
+    rovers[jugadorID].x=9; 
+  }  else{}
+  if(rovers[jugadorID].y<0){ 
+    rovers[jugadorID].y=0; 
   }
-  else if(numY>9){ 
-    numY=9; 
-  }
-  return [numX, numY];
+  else if(rovers[jugadorID].y>9){ 
+    rovers[jugadorID].y=9; 
+  } else {}
 }
 
-function hayObstaculo(numX,numY) {
-  for(kont=0; kont<(obstaculos.length/2); kont+=2){
-    if (numX==obstaculos[kont] & numY ==obstaculos[kont+1]) { //Revisa si el rover coincide con algún obstáculo
-      numX=rover.befX;
-      numY=rover.befY;
+function hayObstaculo() {
+  console.log("despX: "+rovers[jugadorID].x);
+  console.log("despY: "+rovers[jugadorID].y);
+  var kont=0;
+  for(kont=0; kont<(obstaculos.length); kont+=2){
+    if (rovers[jugadorID].x == obstaculos[kont]) {
+      console.log("x entra");
+      if(rovers[jugadorID].y == obstaculos[(kont+1)]) { //Revisa si el rover coincide con algún obstáculo
+      rovers[jugadorID].x=rovers[jugadorID].befX;
+      rovers[jugadorID].y=rovers[jugadorID].befY;
+      console.log("Haz chocado contra un obstáculo");
+      }
     }
   }
-  return [numX,numY];
 }
 
-function Movimiento (rover) {
+function Movimiento () {
 
   //Validar que no se escape del tablero
-  [rover.x,rover.y]=limites(rover.x,rover.y);
+  limites();
 
   //Validar que no esté en un obstáculo
-  [rover.x,rover.y]=hayObstaculo(rover.x,rover.y);
+  hayObstaculo();
 
   //Movimiento en el tablero
-  board[rover.befY][rover.befX] = ' ';//Elimina el rover anterior 
-  board[rover.y][rover.x] = 'X'; //Coloca el rover en la posición actual
-  console.log(board.join("\n")); //imprime en pantalla el tablero actual
+  board[rovers[jugadorID].befY][rovers[jugadorID].befX] = ' ';//Elimina el rover anterior 
+  board[rovers[jugadorID].y][rovers[jugadorID].x] = rovers[jugadorID].marca; //Coloca el rover en la posición actual
+ // console.log(board.join("\n")); //imprime en pantalla el tablero actual
 
   //logear posición del rover
-  rover.travelLog.push("( "+ rover.x +" , " + rover.y +" )");
+  rovers[jugadorID].travelLog.push("( "+ rovers[jugadorID].x +" , " + rovers[jugadorID].y +" )");
 }
